c22b6e405b8e41501605402bbf0811ad98be4e30
esther ayebaza <estherayebaza@gmail.com>
2021-03-24 16:11:32 +0300

    first commit

---
+++ src/mobile.kt
@@ -0,0 +1,28 @@
+fun main() {
+name()
+    var divide=modulus(267,5)
+    println(divide)
+    var sum=addition(342,43,641,342)
+println(sum)
+
+
+    val fact="am wise"
+    println(fact)
+}
+fun name(){
+    val name="Hello Esther"
+    println(name)
+}
+fun modulus(w:Int,d:Int):Int{
+    var divide=w%d
+    return divide
+}
+fun addition(x:Int,y:Int,z:Int,v:Int):Int{
+var sum=x+y+z+v
+    return sum
+}
+
+fun fact(){
+    val fact="am wise"
+    println(fact)
+}
